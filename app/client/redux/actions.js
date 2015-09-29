d7b1351d682f1ca435f38951a3ea3435e19ee737
BinaryMuse <brandon@brandontilley.com>
2015-09-28 22:57:07 -0700

    Concise action creators

--- app/client/redux/actions.js
+++ app/client/redux/actions.js
@@ -1,47 +1,17 @@
-export const selectVideo = (videoId) => {
-  return {
-    type: "SELECT_VIDEO",
-    videoId: videoId
+function actionCreator(type, ...argNames) {
+  return (...args) => {
+    const action = argNames.reduce((acc, argName, idx) => {
+      acc[argName] = args[idx];
+      return acc;
+    }, { type: type });
+    return action;
   };
 }
 
-export const selectNextVideo = () => {
-  return {
-    type: "SELECT_NEXT_VIDEO"
-  };
-};
-
-export const updateCurrentTime = (newTime) => {
-  return {
-    type: "UPDATE_CURRENT_TIME",
-    time: newTime
-  };
-}
-
-export const updatePlaybackStatus = (status) => {
-  return {
-    type: "UPDATE_PLAYBACK_STATUS",
-    status: status
-  };
-}
-
-export const updateVideoDuration = (videoId, duration) => {
-  return {
-    type: "UPDATE_VIDEO_DURATION",
-    videoId, duration
-  };
-};
-
-export const addVideoToPlaylist = (videoId) => {
-  return {
-    type: "ADD_VIDEO",
-    videoId: videoId
-  };
-};
-
-export const removeVideoFromPlaylist = (videoId) => {
-  return {
-    type: "REMOVE_VIDEO",
-    videoId: videoId
-  };
-};
+export const selectVideo = actionCreator("SELECT_VIDEO", "videoId");
+export const selectNextVideo = actionCreator("SELECT_NEXT_VIDEO");
+export const updateCurrentTime = actionCreator("UPDATE_CURRENT_TIME", "newTime");
+export const updatePlaybackStatus = actionCreator("UPDATE_PLAYBACK_STATUS", "status");
+export const updateVideoDuration = actionCreator("UPDATE_VIDEO_DURATION", "videoId", "duration");
+export const addVideoToPlaylist = actionCreator("ADD_VIDEO", "videoId");
+export const removeVideoFromPlaylist = actionCreator("REMOVE_VIDEO", "videoId");
